04ecc0d8ab2750c258d8317a21df118c6a84624a
programmer4christ <devbassiebangura@gmail.com>
2018-11-10 09:02:35 -0500

    Create Vue instance and template.

---
+++ app.js
@@ -0,0 +1,9 @@
+const example = new Vue ({
+    el: "#example",
+    data: {
+        title: "The Bible",
+        author: "The Lord",
+        summary: "The Bible contains all the principles you will ever need to succeed in life. Read it everyday I you will get a glimpse into God's plan and purpose of your life."
+    }
+    
+})
