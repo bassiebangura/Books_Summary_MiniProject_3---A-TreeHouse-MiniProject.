fcedc1a63459d3a27fb2d42f66667381e7096f96
programmer4christ <devbassiebangura@gmail.com>
2018-11-11 02:30:41 -0500

    Update files>

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const example = new Vue ({
-    el: "#example",
+const book = new Vue ({
+    el: "#book",
     data: {
         title: "The Bible",
         author: "The Lord",
-        summary: "The Bible contains all the principles you will ever need to succeed in life. Read it everyday I you will get a glimpse into God's plan and purpose of your life."
+        summary: "The Bible contains all the principles you will ever need to succeed in life."
+    },
+    methods: {
+        sayHello: function () {
+            alert(this.title)
+        }
     }
     
 })
