7c77b13dcfef71dd64dd1e4202478da423d735b0
programmer4christ <devbassiebangura@gmail.com>
2018-11-11 03:29:38 -0500

    Add array with content use to populate to app.js.

--- app.js
+++ app.js
@@ -1,14 +1,65 @@
-const book = new Vue ({
-    el: "#book",
-    data: {
-        title: "The Bible",
-        author: "The Lord",
-        summary: "The Bible contains all the principles you will ever need to succeed in life."
+const media = [
+    {
+      title: 'Hop on Pop', 
+      description: "A delightful children's book.",
+      type: 'book',
+      contributor: 'Dr. Suess',
+      showDetail: false,
     },
-    methods: {
-        sayHello: function () {
-            alert(this.title)
-        }
+    {
+      title: 'The Joy of Painting', 
+      description: "Create a world of happy little trees!",
+      type: 'DVD',
+      contributor: 'Bob Ross',
+      showDetail: false
+    },
+    {
+      title: 'Supernatural: The Complete 12th Season', 
+      description: "A (literally) neverending roadtrip.",
+      type: 'DVD',
+      contributor: "",
+      showDetail: false
+    },
+    {
+      title: 'Planet Earth II', 
+      description: "Hours of beautiful but heart attack-inducing nature footage",
+      type: 'streaming video',
+      contributor: 'David Attenborough',
+      showDetail: false,
+    },
+    {
+      title: 'Titanic', 
+      description: "The boat sinks.",
+      type: 'DVD',
+      contributor: 'James Cameron',
+      showDetail: false,
+    },
+    {
+      title: 'The Sirens of Titan', 
+      description: "Mankind flung its advance agents ever outward, ever outward... it flung them like stones.",
+      type: 'book',
+      contributor: 'Kurt Vonnegut',
+      showDetail: false,
+    },
+    {
+      title: 'Better Call Saul', 
+      description: "A slow-burning Breaking Bad prequel.",
+      type: 'streaming video',
+      contributor: '',
+      showDetail: false,
+    },
+    {
+      title: 'Title 1', 
+      description: "A slow-burning Breaking Bad prequel.",
+      type: 'e-book',
+      contributor: '',
+      showDetail: false,
+    },
+    {
+      title: 'Title 2', 
+      description: "A slow-burning Breaking Bad prequel.",
+      type: 'e-book',
+      contributor: '',
+      showDetail: false,
     }
-    
-})
+  ]
